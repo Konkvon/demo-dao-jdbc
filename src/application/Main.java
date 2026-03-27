cb1b6465b0a03b23811792dd04f26a8c2ddf63a6
João Pedro <joaopedroorem@gmail.com>
2026-03-27 14:44:53 -0300

    Classe Seller

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,12 +1,17 @@
 package application;
 
 import models.entities.Department;
+import models.entities.Seller;
+
+import java.util.Date;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Department obj = new Department(1, "book");
-        System.out.println(obj);
+
+        Seller sl = new Seller(21, "joao", "jao@gmail.com", new Date(), 2000.0, obj);
+        System.out.println(sl);
     }
 }
