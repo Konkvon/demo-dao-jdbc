3bce9596701e3ae758c19c4c1142123b60d80211
João Pedro <joaopedroorem@gmail.com>
2026-03-27 14:34:04 -0300

    classe Department

---
+++ src/application/Main.java
@@ -0,0 +1,12 @@
+package application;
+
+import models.entities.Department;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Department obj = new Department(1, "book");
+        System.out.println(obj);
+    }
+}
